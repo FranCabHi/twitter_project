fcebea6d6bd84501d978d104f91ea42168606a61
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-05-18 21:07:01 -0400

    Adds tweet_hash_tag and hash_tag models (including relationships with tweets), repairs search controller

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,10 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   validates :tw_content, presence: true
+  has_many :tweet_hash_tags
+  has_many :hash_tags, through: :tweet_hash_tags
   
+  after_commit :create_hash_tags, on: :create
   scope :tweets_for_me, -> (following_friends) { where user_id: following_friends }
 
   def tweet_ref
@@ -15,6 +18,16 @@ class Tweet < ApplicationRecord
 
   def user_id
     user.id
+  end
+
+  def create_hash_tags
+    extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+  
+  def extract_name_hash_tags
+    tw_content.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
   end
 end
 # 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_232105) do
+ActiveRecord::Schema.define(version: 2021_05_19_004752) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 2021_05_14_232105) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "hash_tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
@@ -52,6 +58,15 @@ ActiveRecord::Schema.define(version: 2021_05_14_232105) do
     t.datetime "updated_at", null: false
     t.index ["tweet_id"], name: "index_likes_on_tweet_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "tweet_hash_tags", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "hash_tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hash_tag_id"], name: "index_tweet_hash_tags_on_hash_tag_id"
+    t.index ["tweet_id"], name: "index_tweet_hash_tags_on_tweet_id"
   end
 
   create_table "tweets", force: :cascade do |t|
